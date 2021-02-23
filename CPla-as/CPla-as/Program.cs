b05edd39d7125738ff7ec1cd2cfef98a149e96dd
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-24 01:28:24 +0900

    for文の使い方

--- CPla-as/CPla-as/Program.cs
+++ CPla-as/CPla-as/Program.cs
@@ -6,12 +6,10 @@ namespace CPla_as
     {
         static void Main(string[] args)
         {
-            var year = 1;
-            while (year <= 64)
+            for (var year = 1; year <= 64; year++)
             {
                 var westernYear = year + 1925;
-                Console.WriteLine($"昭和{year}年 {westernYear}年");
-                year += 1;
+                Console.WriteLine($"昭和{year} {westernYear}年");
             }
         }
     }
