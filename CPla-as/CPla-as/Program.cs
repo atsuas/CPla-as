92027e5085bca1c1be403f07512ad535e05919f2
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-24 02:14:01 +0900

    複利計算

--- CPla-as/CPla-as/Program.cs
+++ CPla-as/CPla-as/Program.cs
@@ -6,14 +6,26 @@ namespace CPla_as
     {
         static void Main(string[] args)
         {
-            for (var i = 0; i < 50; i++)
+            for (var rate = 0.01m; rate <= 0.10m; rate += 0.01m)
             {
-                if (i % 13 != 0)
+                Console.Write($"rate={rate}:");
+                var sum = 100_000;
+                for (var n = 1; n <= 10; n++)
                 {
-                    continue;
+                    sum += (int)(sum * rate);
+                    if (sum > 200_000)
+                    {
+                        Console.WriteLine($"{n}年後に200,000円を超えました");
+
+                        break;
+                    }
                 }
-                Console.WriteLine($"{i}は13で割り切れる数字です");
+                if (sum <= 200_000)
+                {
+                    Console.WriteLine($"200,000円を超えませんでした。 sum={sum}");
+                }
             }
+            Console.WriteLine("ループを終了しました");
         }
     }
 }
