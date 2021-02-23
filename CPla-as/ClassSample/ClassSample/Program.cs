61dbc92145b1df68e557f665a3f8cd0568499a3e
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-24 04:20:12 +0900

    インスタンスと初期化の簡単な書き方

--- CPla-as/ClassSample/ClassSample/Program.cs
+++ CPla-as/ClassSample/ClassSample/Program.cs
@@ -6,21 +6,18 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var book = new Book();
-            book.Title = "吾輩は猫である";
-            book.Auther = "夏目漱石";
-            book.Pages = 600;
-            book.Rating = 5;
+            var book = new Book
+            {
+                Title = "吾輩は猫である",
+                Auther = "夏目漱石",
+                Pages = 600,
+                Rating = 5
+            };
 
             var title = book.Title;
-            var auther = book.Auther;
-            var pages = book.Pages;
-            var rating = book.Rating;
 
             Console.WriteLine(title);
-            Console.WriteLine(auther);
-            Console.WriteLine(pages);
-            Console.WriteLine(rating);
+            
         }
     }
 
