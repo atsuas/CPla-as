551020607f608467110a87ca4405ff91a2b716ea
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-24 01:03:46 +0900

    switch文の作り方

--- CPla-as/CPla-as/Program.cs
+++ CPla-as/CPla-as/Program.cs
@@ -6,10 +6,27 @@ namespace CPla_as
     {
         static void Main(string[] args)
         {
-            var num = 15;
-            if (num % 3 == 0 || num % 5 == 0)
+            Console.WriteLine("ご希望の時間帯の番号をにゅうりょくしてください");
+            Console.WriteLine("1:9時");
+            Console.WriteLine("2:12時");
+            Console.WriteLine("3:15時");
+            var line = Console.ReadLine();
+            var value = int.Parse(line);
+
+            switch (value)
             {
-                Console.WriteLine("numは3か5でわりきれる");
+                case 1:
+                    Console.WriteLine("9時に予約しました");
+                    break;
+                case 2:
+                    Console.WriteLine("12時に予約しました");
+                    break;
+                case 3:
+                    Console.WriteLine("15時に予約しました");
+                    break;
+                default:
+                    Console.WriteLine("入力された値がおかしいです");
+                    break;
             }
         }
     }
