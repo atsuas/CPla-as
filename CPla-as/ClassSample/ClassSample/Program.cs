e45a6a76e41ab19acc07b4acf30fa28f89ae03e1
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-24 04:01:32 +0900

    bookに代入

--- CPla-as/ClassSample/ClassSample/Program.cs
+++ CPla-as/ClassSample/ClassSample/Program.cs
@@ -6,14 +6,18 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            
+            var book = new Book();
+            book.Title = "吾輩は猫である";
+            book.Auther = "夏目漱石";
+            book.Pages = 600;
+            book.Rating = 5;
         }
     }
 
     class Book
     {
         public string Title { get; set; }
-        public string Aither { get; set; }
+        public string Auther { get; set; }
         public int Pages { get; set; }
         public int Rating { get; set; }
     }
