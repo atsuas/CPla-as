74bae0a54b5fd18570259ca1698c7b641a65a621
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-24 03:09:51 +0900

    2次元配列全ての表示

--- CPla-as/CPla-as/Program.cs
+++ CPla-as/CPla-as/Program.cs
@@ -6,9 +6,21 @@ namespace CPla_as
     {
         static void Main(string[] args)
         {
-            var array2d = new int[4, 3];
-            array2d[3, 2] = 12;
-            Console.WriteLine(array2d[3, 2]);
+            var array2d = new int[,]
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9,},
+                {10,11,12}
+            };
+            for ( var row = 0; row < 4; row++ )
+            {
+                for (var col = 0; col < 3; col++)
+                {
+                    Console.Write("{0} ", array2d[row, col]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
