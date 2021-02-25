a8b93dd36d67cb1e06694864469a8ecf2d11504e
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-26 01:37:54 +0900

    ほしい文字数分だけ取得できる

--- CPla-as/ClassSample/ClassSample/Program.cs
+++ CPla-as/ClassSample/ClassSample/Program.cs
@@ -6,29 +6,12 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var scores = new int[] { 46, 59, 48, 93, 29, 20, 70 };
-            var total = ArrayUtils.Total(scores);   //静的メソッドの呼び出し
-            var average = ArrayUtils.Average(scores);   //静的メソッドの呼び出し
-            Console.WriteLine($"合計:{total}, 平均:{average}");
-        }
-    }
-
-    static class ArrayUtils //staticメソッドで静的クラスにする
-    {
-        public static int Total(int[] numbers)  //配列内の数値の合計を求める
-        {
-            var total = 0;
-            foreach (var n in numbers)
-            {
-                total += n;
-            }
-            return total;
-        }
-
-        public static double Average(int[] numbers)
-        {
-            var total = Total(numbers); //上記のTotalメソッドを呼び出す
-            return (int)total / numbers.Length;
+            var str = "オブジェクト指向";
+            var sub1 = str.Substring(0,6);
+            var sub2 = str.Substring(6, 2);
+            Console.WriteLine(str);
+            Console.WriteLine(sub1);
+            Console.WriteLine(sub2);
         }
     }
 }
