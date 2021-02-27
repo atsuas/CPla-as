d8149d4f133d7c516e79ba3dfdb148e6f17bee54
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-27 21:57:19 +0900

    構造体の定義方法

--- CPla-as/ClassSample/ClassSample/Program.cs
+++ CPla-as/ClassSample/ClassSample/Program.cs
@@ -2,68 +2,54 @@
 
 namespace ClassSample
 {
-    class Program
+    //card構造体
+    struct Card
     {
         static void Main(string[] args)
         {
-            var book1 = new Book { Title = "伊豆の踊子", Auther = "川端康成" };
-            book1.PrintTitle();
-            var book2 = new Book { Title = "走れメロス", Auther = "太宰治" };
-            book2.PrintTitle();
-            var book3 = new Book { Title = "銀河鉄道の夜", Auther = "宮沢賢治" };
-            book3.PrintTitle();
-            Book.ClearCount();
-            Console.WriteLine(Book.Count);
-        }
-    }
-
-    class Book
-    {
-        public static int Count { get; set; }
-
-        public static void ClearCount()
-        {
-            Count = 0;
-        }
-
-        public string Title { get; set; }
-        public string Auther { get; set; }
-        public int Pages { get; set; }
-
-        int _rating;    //フィールドの定義
-
-        public int Rating   //プロパティの定義
-        {
-            //getアクセサ、setアクセサー内は、メソッドと同様、複数行の処理が欠ける
-            get //getアクセサーの定義
+            var card = new Card('S', 8);    //構造体もnew演算子でインスタンスを生成する
+            card.Print();
+            if (card.Suit == 'D')
             {
-                return _rating; //参照時にこのコードが実行される
+                Console.WriteLine("ダイヤです");
             }
-            set //setアクセサーの定義
+            else
             {
-                if (value <= 1) //1以下の値はすべて1をセット
-                {
-                    _rating = 1;
-                }
-                else if (value >= 6) //6以上の値はすべて5をセット
-                {
-                    _rating = 5;
-                }
-                else
-                {
-                    _rating = value;
-                }
+                Console.WriteLine("ダイヤではありません");
             }
         }
 
-        public void PrintTitle()
-        {
-            Console.WriteLine("書籍名: {0}", Title);
-            Count++;
+        public char Suit { get; private set; }  //読み取り専用プロパティ
+        public int Number { get; private set; } //読み取り専用プロパティ
 
-            Console.WriteLine(Count);
+        //コンストラクター
+        public Card(char suit, int number)
+        {
+            Suit = suit;
+            Number = number;
         }
-        
+
+        //メソッド
+        public void Print()
+        {
+            var s = "";
+            switch (Suit)
+            {
+                case 'H':
+                    s = "ハート";
+                    break;
+                case 'D':
+                    s = "ダイヤ";
+                    break;
+                case 'S':
+                    s = "スペード";
+                    break;
+                case 'C':
+                    s = "クラブ";
+                    break;
+            }
+            Console.WriteLine($"{s} {Number}");
+        }
     }
 }
 
