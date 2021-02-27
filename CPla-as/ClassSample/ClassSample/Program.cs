ca1b799d19ef1416ab0d81296ebf6dfe0bbdd5d4
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-28 03:27:50 +0900

    クラスの継承

--- CPla-as/ClassSample/ClassSample/Program.cs
+++ CPla-as/ClassSample/ClassSample/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;  // LINQを使うのに必要
+using System.Linq;
 
 namespace ClassSample
 {
@@ -8,11 +8,47 @@ namespace ClassSample
     {
         static void Main()
         {
-            var nums = new int[] { 62, 4, 34, 26, 15, 51, 97, 84, 72, };
-            var list = nums.Where(x => x <= 10)
-                            .ToList();    //Whereの結果をList<int>に変換する
-            Console.WriteLine(list[0]);
+            var employee = new Employee
+            {
+                Number = 368,
+                FirstName = "ともや",
+                LastName = "鈴木",
+                HireDate = new DateTime(2016, 10, 3)
+            };
+            Console.WriteLine("従業員番号{0}の{1}は、{2}年に入社しました",
+                employee.Number, employee.FullName, employee.HireDate.Year);
         }   
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get { return LastName + FirstName; }
+        }
+        public string Email { get; set; }
+    }
+
+    //従業員クラス
+    class Employee : Person    //Personクラスを継承
+    {
+        //従業員番号
+        public int Number { get; set; }
+        //入社年月日
+        public DateTime HireDate { get; set; }
+    }
+
+    //顧客クラス
+    class Customer : Person
+    {
+        //顧客ID
+        public string Id { get; set; }
+        //顧客ランク
+        public int Rank { get; set; }
+        //クレジットカード番号
+        public string CreditCardNumber { get; set; }
     }
 }
 
