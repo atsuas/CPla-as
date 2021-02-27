9c4b56c52cf5a0bb9912b27dc9a9f0dbb8f8d01b
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-27 20:24:27 +0900

    プロパティに特別な動作を加える

--- CPla-as/ClassSample/ClassSample/Program.cs
+++ CPla-as/ClassSample/ClassSample/Program.cs
@@ -6,31 +6,54 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            Console.Write("名前を入力してください⇒");
-            var name = Console.ReadLine();
-            var mypet = new VirtualPet(name);   //引数を指定したコンストラクターが呼ばれる
-            Console.WriteLine($"Name: {mypet.Name}");
-            Console.WriteLine($"Mood: {mypet.Mood}");
-            Console.WriteLine($"Energy: {mypet.Energy}");
-
-        }
-
-        class VirtualPet
-        {
-            public string Name { get; set; }
-            public int Mood { get; set; }
-            public int Energy { get; set; }
-
-            public VirtualPet(string name) //コンストラクターの名前はクラスと同じにする
-            {
-                Name = name;
-                Mood = 5;
-                Energy = 100;
-            }
+            var book1 = new Book { Title = "伊豆の踊子", Auther = "川端康成" };
+            book1.PrintTitle();
+            var book2 = new Book { Title = "走れメロス", Auther = "太宰治" };
+            book2.PrintTitle();
+            var book3 = new Book { Title = "銀河鉄道の夜", Auther = "宮沢賢治" };
+            book3.PrintTitle();
+            Book.ClearCount();
+            Console.WriteLine(Book.Count);
         }
     }
-}
 
+    class Book
+    {
+        public static int Count { get; set; }
+
+        public static void ClearCount()
+        {
+            Count = 0;
+        }
+
+        public string Title { get; set; }
+        public string Auther { get; set; }
+        public int Pages { get; set; }
+
+        int _rating;    //フィールドの定義
+
+        public int Rating   //プロパティの定義
+        {
+            get //getアクセサーの定義
+            {
+                return _rating; //参照時にこのコードが実行される
+            }
+            set //setアクセサーの定義
+            {
+                _rating = value;    //代入時にこのコードが実行される
+            }
+        }
+
+        public void PrintTitle()
+        {
+            Console.WriteLine("書籍名: {0}", Title);
+            Count++;
+
+            Console.WriteLine(Count);
+        }
+        
+    }
+}
 
 
 
