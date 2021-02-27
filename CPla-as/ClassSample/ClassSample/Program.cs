6b17d4282de4b0d82f9a96c81eb7a7129a801142
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-28 06:41:03 +0900

    String型、Tostringについて

--- CPla-as/ClassSample/ClassSample/Program.cs
+++ CPla-as/ClassSample/ClassSample/Program.cs
@@ -8,7 +8,8 @@ namespace ClassSample
     {
         static void Main()
         {
-            var employee = new Employee
+            //呼び出し
+            var employee = new Employee()
             {
                 Number = 368,
                 FirstName = "ともや",
@@ -16,7 +17,8 @@ namespace ClassSample
                 Email = "tomotomo@example.com",
                 HireDate = new DateTime(2016, 10, 3)
             };
-            employee.Print();   //オーバライドしたEmployeeクラスのPrintメソッドを呼び出す
+            var s = employee.ToString();    //オーバーライドしたToStringメソッドを呼び出す
+            Console.WriteLine(s);
 
             var person = new Person
             {
@@ -24,7 +26,7 @@ namespace ClassSample
                 LastName = "佐々木",
                 Email = "sasasa@example.com"
             };
-            person.Print(); //PersonクラスのPrintメソッドを呼び出す
+            person.Print();
         }   
     }
 
@@ -38,7 +40,7 @@ namespace ClassSample
         }
         public string Email { get; set; }
 
-        public virtual void Print() //virtualキーワードをつけるとサブクラスでオーバライドできる
+        public virtual void Print()
         {
             Console.WriteLine($"名前: {FullName} ({Email})");
         }
@@ -49,10 +51,10 @@ namespace ClassSample
         public int Number { get; set; }
         public DateTime HireDate { get; set; }
 
-        // overrideキーワードでメソッドを上書きできる
-        public override void Print()
+        public override string ToString()   //ToStringメソッドをオーバーライド
         {
-            Console.WriteLine($"{Number}:{FullName}({Email}) {HireDate.Year}年入社");
+            var s = $"{Number} {FullName} " + $"{HireDate.Year}年{HireDate.Month}月{HireDate.Day}日入社";
+            return s;
         }
     }
 
