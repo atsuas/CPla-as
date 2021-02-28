33973bc954fddc1f013d2045e384430b44be23f9
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-02-28 09:49:28 +0900

    例外をキャッチする

--- CPla-as/ClassSample/ClassSample/Program.cs
+++ CPla-as/ClassSample/ClassSample/Program.cs
@@ -6,7 +6,19 @@ namespace ClassSample
     {
         static void Main()
         {
-            
+            try
+            {
+                var total = 1000;
+                var line = Console.ReadLine();
+                var count = int.Parse(line);
+                var ans = total / count;
+                Console.WriteLine(ans);
+                Console.WriteLine("正常終了");
+            }
+            catch
+            {
+                Console.WriteLine("入力した値が正しくありません");
+            }
         }   
     }
 }
